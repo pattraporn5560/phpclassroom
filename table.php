46f5345373b486e115aaa9217bd59d9eb41e4df6
pattraporn5560 <92217176+pattraporn5560@users.noreply.github.com>
2021-11-16 12:32:56 +0700

    Update table.php

--- table.php
+++ table.php
@@ -1,15 +1,20 @@
-<?php
-  echo "<table border='1'>"; //tagเปิด
-    
-  for($i=0;$i<10;$i++){
-    echo "<tr>";
-    echo "<td>คอลัมน์</td>";
-    echo "<td>คอลัมน์</td>";
-    echo "<td>คอลัมน์</td>";
-    echo "<td>คอลัมน์</td>";
-    echo "</tr>";  
-  }
-  
-  echo "</table>"; //tagปิด
-
-?>
+<html>
+  <head></head>
+  <body>
+    <table border="1">
+      <?php
+        $row = 100;
+        for($i=0;$i<$row;$i++){
+          echo "<tr>";
+          echo "<td>column 1</td>";
+          echo "<td>column 2</td>";
+          echo "<td>column 3</td>";
+          echo "<td>column 4</td>";
+          echo "<td>column 5</td>";
+          echo "<td>column 6</td>";
+          echo "</tr>";
+        }
+      ?>
+    </table>
+  </body>
+</html>
