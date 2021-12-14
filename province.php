31b9837fec102a3bc6cebf94a3597ac0528c634e
pattraporn5560 <92217176+pattraporn5560@users.noreply.github.com>
2021-12-14 09:43:24 +0700

    Update province.php

--- province.php
+++ province.php
@@ -1,10 +1,9 @@
-<php
+<?php
 
   $json = file_get_contents("https://covid19.ddc.moph.go.th/api/Cases/today-cases-by-provinces");
   $data = json_decode($json);
-     
+  
   foreach($data as $key=>$val){
     echo $val->province."<br>";
-  }
   
 ?>
