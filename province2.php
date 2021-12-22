fba8768862557fbaf64681ca3d51b9f224e52a29
pattraporn5560 <92217176+pattraporn5560@users.noreply.github.com>
2021-12-22 21:52:05 +0700

    Update province2.php

--- province2.php
+++ province2.php
@@ -4,7 +4,7 @@ $json = file_get_contents("https://covid19.ddc.moph.go.th/api/Cases/today-cases-
 
   echo "<center><b>รายงานสถานการโควิด-19 แยกจังหวัด</b></center>";
   echo "<table border='1px'>";
-  echo "<tr align='center'><td bgcolor='#FFE4E1'><b>ลำดับ</b></td><td bgcolor='#FFDEAD'><b>จังหวัด</b></td><td bgcolor='#FFE4E1'><b>ยอดผู้ป่วยใหม่</b></td><td bgcolor='#FFDEAD'><b>ยอดผู้ป่วยรวม</b></td><td bgcolor='#FF66CC'><b>ยอดผู้ป่วยใหม่ภายในประเทศ</b></td><td bgcolor='#CCCCFF'><b>ยอดผู้ป่วยรวมในประเทศ</b></td>
+  echo "<tr align='center'><td bgcolor='#FFE4E1'><b>ลำดับ</b></td><td bgcolor='#FFDEAD'><b>จังหวัด</b></td><td bgcolor='#FFE4E1'><b>ยอดผู้ป่วยใหม่</b></td><td bgcolor='#FFDEAD'><b>ยอดผู้ป่วยรวม</b></td><td bgcolor='#FFE4E1'><b>ยอดผู้ป่วยใหม่ภายในประเทศ</b></td><td bgcolor='#FFDEAD'><b>ยอดผู้ป่วยรวมในประเทศ</b></td>
   <td bgcolor='#FFE4E1'><b>ยอดผู้เสียชีวิตใหม่</b></td><td bgcolor='#FFDEAD'><b>ยอดผู้เสียชีวิตรวม</b></td><td bgcolor='#FFE4E1'><b>วันที่</b></td></tr>";
   foreach($data as $key=>$val){
     echo "<tr align='center'>";
