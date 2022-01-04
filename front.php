4a79627b7759bad4af35b3ab27923839504090e0
pattraporn5560 <92217176+pattraporn5560@users.noreply.github.com>
2022-01-04 09:14:42 +0700

    Update front.php

--- front.php
+++ front.php
@@ -9,6 +9,19 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
     <title>Hello, world!</title>
+    
+      <style>
+        #col1{
+          background-color:red;
+        }
+        #col2{
+          background-color:green;
+        }
+        #col3{
+          background-color:blue;
+        }
+      </style>
+    
   </head>
   <body>
     
