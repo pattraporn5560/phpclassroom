87f266589e7d391e00e16c2da061766fc59ce170
pattraporn5560 <92217176+pattraporn5560@users.noreply.github.com>
2022-02-18 13:49:29 +0700

    Update project.php

--- project.php
+++ project.php
@@ -1,3 +1,7 @@
+<?php 
+  $json = file_get_contents("https://covid19.ddc.moph.go.th/api/Cases/today-cases-all");
+  $data = json_decode($json);
+?>
 <!doctype html>
 <html lang="en">
   <head>
@@ -7,9 +11,12 @@
     <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
     <meta name="generator" content="Hugo 0.88.1">
     <title>ศูนย์ข้อมูล Covid-19 โรงเรียนหล่มสักวิทยาคม</title>
+
     <link rel="canonical" href="https://getbootstrap.com/docs/5.1/examples/pricing/">
+
     <!-- Bootstrap core CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
     <style>
       .bd-placeholder-img {
         font-size: 1.125rem;
@@ -18,17 +25,20 @@
         -moz-user-select: none;
         user-select: none;
       }
+
       @media (min-width: 768px) {
         .bd-placeholder-img-lg {
           font-size: 3.5rem;
         }
       }
     </style>
+
     
     <!-- Custom styles for this template -->
     <link href="https://getbootstrap.com/docs/5.1/examples/pricing/pricing.css" rel="stylesheet">
   </head>
   <body>
+
 <div class="container py-3">
   <header>
     <div class="d-flex flex-column flex-md-row align-items-center pb-3 mb-4 border-bottom">
@@ -41,13 +51,13 @@
       <div class="card mb-3" style="max-width: 540px;">
         <div class="row g-0">
           <div class="col-md-4">
-            <img src="profile.jpg" class="img-fluid rounded-start" alt="...">
+            <img src="/E12E0125-A083-46C7-B5F2-9462ECFD91E6.jpeg" class="img-fluid rounded-start" alt="...">
           </div>
           <div class="col-md-8">
             <div class="card-body">
               <h5 class="card-title">จัดทำโดย</h5>
-              <p class="card-text">นายยิ่งคุณ รอดทิม </p>
-              <p class="card-text"><small class="text-muted">ม.6/13 เลขที่ 0 , 88888</small></p>
+              <p class="card-text">นางสาวปวริศา สีตะระโส </p>
+              <p class="card-text"><small class="text-muted">ม.6/13 เลขที่ 20 , 55803</small></p>
             </div>
           </div>
         </div>
@@ -55,7 +65,7 @@
     </div>
     <div class="pricing-header p-3 pb-md-4 mx-auto text-center">
       <h1 class="display-7 fw-normal">รายงานข้อมูล Covid-19 รายวัน</h1>
-      <p class="fs-5 text-muted">ข้อมูล ณ วันที่ xx เดือน xxxx ปี 2565</p>
+      <p class="fs-5 text-muted">ข้อมูล ณ <?php echo $data[0]->txn_date; ?></p>
     </div>
   </header>
   <!-- แก้ไขส่วนที่ 2 -->
@@ -67,7 +77,7 @@
             <h4 class="my-0 fw-normal">ผู้ป่วยใหม่</h4>
           </div>
           <div class="card-body">
-            <h1 class="card-title pricing-card-title">xxx<small class="text-muted fw-light"> คน</small></h1>
+            <h1 class="card-title pricing-card-title"><?php echo $data[0]->new_case; ?><small class="text-muted fw-light"> คน</small></h1>
           </div>
         </div>
       </div>
@@ -77,7 +87,7 @@
             <h4 class="my-0 fw-normal">หายป่วยกลับบ้าน</h4>
           </div>
           <div class="card-body">
-            <h1 class="card-title pricing-card-title">xxx<small class="text-muted fw-light"> คน</small></h1>
+            <h1 class="card-title pricing-card-title"><?php echo $data[0]->new_case_excludeabroad; ?><small class="text-muted fw-light"> คน</small></h1>
           </div>
         </div>
       </div>
@@ -87,7 +97,7 @@
             <h4 class="my-0 fw-normal">ผู้ป่วยสะสม</h4>
           </div>
           <div class="card-body">
-            <h1 class="card-title pricing-card-title">xxx<small class="text-muted fw-light"> คน</small></h1>
+            <h1 class="card-title pricing-card-title"><?php echo $data[0]->total_case; ?><small class="text-muted fw-light"> คน</small></h1>
           </div>
         </div>
       </div>
@@ -97,12 +107,14 @@
             <h4 class="my-0 fw-normal">เสียชีวิต</h4>
           </div>
           <div class="card-body">
-            <h1 class="card-title pricing-card-title">xxx<small class="text-muted fw-light"> คน</small></h1>
+            <h1 class="card-title pricing-card-title"><?php echo $data[0]->new_death; ?><small class="text-muted fw-light"> คน</small></h1>
           </div>
         </div>
       </div>
     </div>
+
   </main>
+
   <footer class="pt-4 my-md-5 pt-md-5 border-top">
     <header>
       <div class="pricing-header p-3 pb-md-4 mx-auto text-center">
@@ -116,42 +128,49 @@
             <th>#</th>
             <th>จังหวัด</th>
             <th>ผู้ป่วยใหม่</th>
-            <th>หายป่วยกลับบ้าน</th>
+            <th>ผู้เสียชีวิตรวม</th>
             <th>ผู้ป่วยสะสม</th>
             <th>เสียชีวิต</th>
           </tr>
           <!-- แก้ไขส่วนที่ 3 -->
           <!-- Loop -->
-          <tr>
-            <td> - </td>
-            <td> - </td>
-            <td> - </td>
-            <td> - </td>
-            <td> - </td>
-            <td> - </td>
-          </tr>
-          <tr>
-            <td> - </td>
-            <td> - </td>
-            <td> - </td>
-            <td> - </td>
-            <td> - </td>
-            <td> - </td>
-          </tr>
-          <tr>
-            <td> - </td>
-            <td> - </td>
-            <td> - </td>
-            <td> - </td>
-            <td> - </td>
-            <td> - </td>
-          </tr>
+          
+  <?php   
+          $json = file_get_contents("https://covid19.ddc.moph.go.th/api/Cases/today-cases-by-provinces");
+  $data = json_decode($json);
+          foreach($data as $key=>$val){
+     echo "<tr>";
+    echo "<td bgcolor ='peachpuff'>";
+    echo ($key+1);
+    echo "</td>";
+    echo "<td>";
+    echo $val->province;
+    echo "</td>";
+echo "<td>";
+    echo $val->new_case;
+    echo "</td>";
+    echo "<td>";
+    echo $val->total_death;
+    echo "</td>";
+    echo "<td>";
+    echo $val->total_case_excludeabroad;
+    echo "</td>";
+    echo "<td>";
+    echo $val->new_death;
+    echo "</td>";
+    echo "<td>";
+   
+    echo "</tr>";
+  }
+          ?>
         </table>
       </div>
     </div>
+
   </footer>
-  <!-- แก้ไขส่วนที่ 4 -->
 </div>
+
+
     
   </body>
 </html>
